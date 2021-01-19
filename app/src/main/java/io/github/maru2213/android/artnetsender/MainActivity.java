2f3abd4e1d8e925a256ccb2d3e5cb0bcb4d013ad
maru2213 <25478531+maru2213@users.noreply.github.com>
2021-01-19 21:44:03 +0900

    refactor: rename file

--- app/src/main/java/io/github/maru2213/android/artnetsender/MainActivity.java
+++ app/src/main/java/io/github/maru2213/android/artnetsender/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_dark);
 
         currentMode = NORMAL_MODE;
 
